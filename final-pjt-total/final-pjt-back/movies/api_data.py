fe10ae94865cfdd6115aba0931a29b6bc5a1e312
TaeHeon <ggnsall@naver.com>
2022-11-18 17:49:07 +0900

    데이터 500개 받기

--- final-pjt-total/final-pjt-back/movies/api_data.py
+++ final-pjt-total/final-pjt-back/movies/api_data.py
@@ -2,37 +2,37 @@ import requests
 import json
 
 # # # popularMovie - url
-# API_KEY = '42584510a0a43e09681fec8c6f36f050'
-# language = 'ko-kr'
-# page = '1'
-# url = 'https://api.themoviedb.org/3/movie/popular'
+API_KEY = '42584510a0a43e09681fec8c6f36f050'
+language = 'ko-kr'
+page = '1'
+url = 'https://api.themoviedb.org/3/movie/popular'
 
-# def get_data(url, PAGE):
-#     API_URL = f'{url}?api_key={API_KEY}&language={language}&page={PAGE}'
-#     return API_URL
+def get_data(url, PAGE):
+    API_URL = f'{url}?api_key={API_KEY}&language={language}&page={PAGE}'
+    return API_URL
 
 
-# new_list = []
-# for i in range(1,5):
-#     get_data(url, i)
-#     response = requests.get(get_data(url,i)).json()
+new_list = []
+for i in range(5,30):
+    get_data(url, i)
+    response = requests.get(get_data(url,i)).json()
 
-#     for data in response['results']:
-#         new_data = {'model' : 'movies.movie'}
-#         new_data['pk'] = data['id']
-#         new_data['fields'] = {}
-#         new_data['fields']['title'] = data['title']
-#         new_data['fields']['release_date'] = data['release_date']
-#         new_data['fields']['adult'] = data['adult']
-#         new_data['fields']['overview'] = data['overview']
-#         new_data['fields']['popularity'] = data['popularity']
-#         new_data['fields']['poster_path'] = data['poster_path']
-#         # new_data['fields']['innermovie_id'] = data['id']
-#         new_list.append(new_data)
-#         print(new_data)
+    for data in response['results']:
+        new_data = {'model' : 'movies.movie'}
+        new_data['pk'] = data['id']
+        new_data['fields'] = {}
+        new_data['fields']['title'] = data['title']
+        new_data['fields']['release_date'] = data['release_date']
+        new_data['fields']['adult'] = data['adult']
+        new_data['fields']['overview'] = data['overview']
+        new_data['fields']['popularity'] = data['popularity']
+        new_data['fields']['poster_path'] = data['poster_path']
+        # new_data['fields']['innermovie_id'] = data['id']
+        new_list.append(new_data)
+        print(new_data)
 
-# with open('movies/fixtures/movies.json', 'a', encoding='UTF-8') as f:
-#     json.dump(new_list, f, ensure_ascii=False, indent=2)
+with open('movies/fixtures/movies.json', 'a', encoding='UTF-8') as f:
+    json.dump(new_list, f, ensure_ascii=False, indent=2)
 
 
 # # genre - url
