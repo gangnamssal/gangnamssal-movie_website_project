29cc0726b10b2f7e9893e8b3da399fa173b297ac
ajin <lany1515@naver.com>
2022-11-23 01:04:21 +0900

    home화면 수정

--- final-pjt-total/final-pjt-front/src/main.js
+++ final-pjt-total/final-pjt-front/src/main.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
-
+import Carousel3d from 'vue-carousel-3d'
 Vue.config.productionTip = false
-
+Vue.use(Carousel3d);
 new Vue({
   store,
   router,
